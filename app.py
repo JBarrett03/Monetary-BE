4c04aa485fc84b21aa93c1604aec75406c1a27eb
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-06 01:19:46 +0000

    Creating login

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from datetime import UTC, datetime
 from flask import Flask, jsonify, make_response, request
-import uuid, random
 from pymongo import MongoClient
 from bson import ObjectId
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
 
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.monetary    # Select the database
@@ -84,6 +87,19 @@ def deleteUser(id):
         return make_response(jsonify( {} ), 204)
     else:
         return make_response(jsonify({ "error": "Invalid user ID" }), 404)
+
+@app.route("/api/v1.0/login", methods=['GET'])
+def login():
+    auth = request.authorization
+    if auth and auth.password == 'password':
+        token = jwt.encode({
+            'user': auth.username,
+            'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=30)
+            },
+            app.config['SECRET_KEY'],
+            algorithm='HS256')
+        return make_response(jsonify({ 'token': token }), 200)
+    return make_response('Could not verify', 401, { 'WWW-Authenticate': 'Basic realm = "Login required"' })
 
 if __name__ == '__main__':
     app.run(debug=True)
