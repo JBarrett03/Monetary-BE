6ee208a460130db5f9ab5b58ef13412fb4e9bf74
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-06 00:13:45 +0000

    Retrieving all users

--- app.py
+++ app.py
@@ -1,10 +1,64 @@
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def index():
-    return make_response("<h1> Hello World </h1>", 200)
+users = [
+    {
+    "_id": {
+        "$oid": "697540596bd2ce0e5cf96566"
+    },
+    "firstName": "John",
+    "lastName": "Doe",
+    "email": "john.doe@example.com",
+    "password": "password",
+    "phone": "123-456-7890",
+    "address": "123 Main St, Anytown, USA",
+    "DOB": "1990-01-01",
+    "emailVerified": True,
+    "phoneVerified": False,
+    "status": "active",
+    "createdAt": "2025-01-22T10:00:00Z",
+    "lastLoginAt": "2025-01-22T10:00:00Z"
+    },
+    {
+    "_id": {
+        "$oid": "697540596bd2ce0e5cf96567"
+    },
+    "firstName": "Jane",
+    "lastName": "Smith",
+    "email": "jane.smith@example.com",
+    "password": "password",
+    "phone": "123-456-7890",
+    "address": "123 Main St, Anytown, USA",
+    "DOB": "1990-01-01",
+    "emailVerified": True,
+    "phoneVerified": True,
+    "status": "active",
+    "createdAt": "2025-01-22T10:00:00Z",
+    "lastLoginAt": "2025-01-22T10:00:00Z"
+    },
+    {
+    "_id": {
+        "$oid": "697540596bd2ce0e5cf96568"
+    },
+    "firstName": "Bob",
+    "lastName": "Johnson",
+    "email": "bob.johnson@example.com",
+    "password": "password",
+    "phone": "123-456-7890",
+    "address": "123 Main St, Anytown, USA",
+    "DOB": "1990-01-01",
+    "emailVerified": False,
+    "phoneVerified": True,
+    "status": "active",
+    "createdAt": "2025-01-22T10:00:00Z",
+    "lastLoginAt": "2025-01-22T10:00:00Z"
+    }
+]
 
-if __name__ == "__main__":
+@app.route("/api/v1.0/users", methods=["GET"])
+def getUsers():
+    return make_response(jsonify(users), 200)
+
+if __name__ == '__main__':
     app.run(debug=True)
