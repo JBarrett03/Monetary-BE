ffa9a632ae85b6fc9e4a3c03f6fe0e3f42cb3e75
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-08 16:52:05 +0000

    Enabling flask CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from blueprints.users.users import users_bp
 from blueprints.accounts.accounts import accounts_bp
 from blueprints.transactions.transactions import transactions_bp
 from blueprints.auth.auth import auth_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(users_bp)
 app.register_blueprint(accounts_bp)
 app.register_blueprint(transactions_bp)
